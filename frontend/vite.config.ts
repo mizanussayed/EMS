7f7498f74359805f2b0dc95b1fcce7f8fb7b49a4
Mizanus Sayed <mizandusp@gmail.com>
2026-05-13 22:56:33 +0600

    upate index

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -18,10 +18,8 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  base: '/EMS/',
   plugins: [
-    figmaAssetResolver(),
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
