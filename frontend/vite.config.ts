fb86ae3a611d6059b304d17958c15df3a5941767
Mizanus Sayed <mizandusp@gmail.com>
2026-05-24 11:16:03 +0600

    add _redirects

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,19 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 
-function figmaAssetResolver() {
-  return {
-    name: 'figma-asset-resolver',
-    resolveId: (id: string) => {
-      if (id.startsWith('figma:asset/')) {
-        const filename = id.replace('figma:asset/', '')
-        return path.resolve(__dirname, 'src/assets', filename)
-      }
-      return null;
-    },
-  }
-}
-
 export default defineConfig({
   plugins: [
     react(),
